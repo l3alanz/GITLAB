34eaa8662f69152bf1aa60c4c9f2084f63773ab7
l3alanz <l3alanz@hotmail.com>
2016-04-27 12:00:27 +0700

    Modified Lottery.c

--- Lottery.c
+++ Lottery.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 void main()
 {
-  int WinLotteryNo[10] = {1522, 1711, 5515, 7233, 5614, 8876, 1215, 4232, 9155, 9995}; 
+  int WinNo[10] = {1522, 1711, 5515, 7233, 5614, 8876, 1215, 4232, 9155, 9995}; 
   float WinAmount[10] = {50000, 10000, 10000, 5000, 5000, 5000, 5000, 3000, 3000, 3000};
   int N = 10;
   int i, won; 
@@ -15,7 +15,7 @@ void main()
     won = 0; 
     for (i=0;i<N && !won; i++) 
     {
-       if (YourLotteryNo == WinLotteryNo[i]) 
+       if (YourLotteryNo == WinNo[i]) 
        {
           printf("Congratulations You Won %8.2f baht\n", WinAmount[i]); 
           won = 1;
@@ -26,6 +26,6 @@ void main()
     printf("Enter your Lottery Number (0 to end): "); 
     scanf("%d", &YourLotteryNo); 
   }
-  printf("GOODBYE");
+  printf("BYE");
   scanf("%d", &i);
 }
