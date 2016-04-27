313f472ac7da61f7ef08b273554237db6e44d128
l3alanz <l3alanz@hotmail.com>
2016-04-27 12:23:22 +0700

    Modify Lottery.c

--- Lottery.c
+++ Lottery.c
@@ -22,8 +22,8 @@ void main()
        }
     }
     if (!won) 
-      printf("Sorry Lottery Number %4d did not win\n", YourLotteryNo); 
-    printf("Enter your Lottery Number (0 to end): "); 
+      printf("Sorry your Lottery Number : %4d did not win\n", YourLotteryNo); 
+    printf("Please,Enter your Lottery Number (0 to end): "); 
     scanf("%d", &YourLotteryNo); 
   }
   printf("BYE");
